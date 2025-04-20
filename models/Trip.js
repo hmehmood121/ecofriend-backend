b0494a08f26bfdea56750d4f58a82835db1d3c84
Hasan Mehmood <hasanmehmood@Hasans-MacBook-Air.local>
2025-04-20 11:23:11 +0500

    new fields added

--- models/Trip.js
+++ models/Trip.js
@@ -1,38 +1,19 @@
 const mongoose = require('mongoose');
 
 const tripSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true
-  },
-  tripId: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  startTime: {
-    type: Date,
-    required: true
-  },
-  endTime: {
-    type: Date,
-    required: true
-  },
-  Distance: {
-    type: Number,
-    required: true
-  },
-  fuelConsumption: {
-    type: Number,
-    required: true
-  },
-  ecoDrivingScore: {
-    type: Number,
-    required: true
-  }
-}, { timestamps: true });
+  userId: { type: String, required: true },
+  tripId: { type: String, required: true },
+  name: { type: String, required: true },
+  startTime: { type: Date, required: true },
+  endTime: { type: Date, required: true },
+  Distance: { type: Number, required: true },
+  fuelConsumption: { type: Number, required: true },
+  ecoDrivingScore: { type: Number, required: true },
+  vehicle_id: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  vehicle_type: { type: String, required: true },
+  speed: { type: Number, required: true }
+});
 
 module.exports = mongoose.model('Trip', tripSchema);
